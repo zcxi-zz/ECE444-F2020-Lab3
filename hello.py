029f52d65289ff80f47a8e9c8758907bf90c1d68
Chenxi Zhao <chenxizhao2008@gmail.com>
2020-10-01 14:55:51 -0600

    Added field for submitting email address

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@ from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.validators import Email
+from wtforms.validators import Regexp
+import re
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -14,6 +17,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired(), Email(), Regexp('[A-Za-z0-9@._%+-]{1,200}\.utoronto\.ca$', message="Must be a valid Utoronto address")])
     submit = SubmitField('Submit')
 
 
@@ -32,5 +36,9 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
